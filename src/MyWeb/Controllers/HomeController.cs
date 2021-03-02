f03298848ae6c3f01edc547a029ed925017eff84
Worameth Semapat <worameth.semapat@gmail.com>
2021-03-02 10:03:10 +0700

    Update dotnet-package.yaml

--- src/MyWeb/Controllers/HomeController.cs
+++ src/MyWeb/Controllers/HomeController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Threading;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MyWeb.Models;
